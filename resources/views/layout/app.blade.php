cb3745b7fec860f60a091a0c149e44ae9dbb987d
ugyentshering <ugyen.t@gmail.com>
2019-08-09 11:33:41 +0600

    npm

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -8,6 +8,8 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        
+    <link rel="stylesheet", href="{{mix('css/app.css')}}">
 
         <!-- Styles -->
         <style>
@@ -95,5 +97,6 @@
                 </div>
             </div>
         </div>
+    <script src="{{mix('js/app.js')}}"></script>
     </body>
 </html>
